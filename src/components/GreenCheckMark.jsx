8cf006516ddac57b1f5a96c8c03742f39cac154e
unknown <dockadams333@gmail.com>
2023-07-29 13:24:49 +0700

    Progress/checkmark component toggle

---
+++ src/components/GreenCheckMark.jsx
@@ -0,0 +1,9 @@
+function GreenCheckMark(){
+    return(
+        <span className="check-container">
+            <div className="greenCheckMark"></div>
+        </span>
+    )
+}
+
+export default GreenCheckMark;
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import InputField from './InputField';
 import ButtonContainer from './ButtonContainer';
 import ProgressBar from './ProgressBar';
+import GreenCheckMark from './GreenCheckMark';
 import "../styles/Section.css";
 
 function Section({ sectionName }){
@@ -12,23 +13,23 @@ function Section({ sectionName }){
         const inputValue = e.target.value;
         setInputValues((prev) => ({ ...prev, [key]: inputValue}))
     }
-    const handleFocusChange = (focused) => {
-        console.log(focused);
-        setIsAnyInputFocused(focused);
-    }
-    useEffect(() => {
+    const handleFocusChange = (focused) => setIsAnyInputFocused(focused)
+    const currentProgressBar = () => {
         const totalInputs = Object.keys(sectionName).length - 1;
         const completedInputs = Object.values(inputValues).filter(Boolean).length;
         const progressPercent = (completedInputs / totalInputs) * 100;
-        if(progressPercent === 100){
-            console.log("Finished");
-        }
+        return progressPercent;
+    }
+        
+    useEffect(() => {
+        const progressPercent = currentProgressBar();
         setProgress(progressPercent);
-      }, [inputValues, sectionName]);
+      }, [inputValues, sectionName, isAnyInputFocused]);
+
     return(
         <section>
             <h2>{sectionName["Title"]}</h2>
-            <ProgressBar Progress={progress} />
+            {currentProgressBar() === 100 && !isAnyInputFocused  ? <GreenCheckMark /> : <ProgressBar Progress={progress} />}
             {Object.entries(sectionName).map(([key, value], index) => {
                 if(key === "Title") return null;
                 return(
